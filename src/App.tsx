794cf13d76b8797e0819594ebba0545352a19196
BenWowo <benjaminnwowo@gmail.com>
2024-01-28 04:50:44 -0600

    create client

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <main>
+        <Button>This is the button</Button>
+      </main>
+    </div>
+  );
+}
--- usage.md
+++
@@ -1,8 +0,0 @@
-python audio_model <audio_path>
-  prints value of [0, 1] to std output
-
-python text_model_phishing <text_path>
-
-python text_model_generate <text_path>
-
-python text_model_generated <text_path>
